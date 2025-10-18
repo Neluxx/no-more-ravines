f95147bea08dfaa663ffc24f24e4eec139188e03
fabian.arndt <fabian.arndt@orca.ch>
2025-10-18 15:28:10 +0200

    Migrate datapack to fabric mod

---
+++ src/client/java/neluxx/no_more_ravines/NoMoreRavinesClient.java
@@ -0,0 +1,10 @@
+package neluxx.no_more_ravines;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoMoreRavinesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
