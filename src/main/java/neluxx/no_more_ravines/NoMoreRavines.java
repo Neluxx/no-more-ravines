9861e8f17c8277481059628f4287f42540b95b34
Neluxx <fabian.arndt96@proton.me>
2025-12-08 20:17:42 +0100

    refactor: remove unnecessary logger

--- src/main/java/neluxx/no_more_ravines/NoMoreRavines.java
+++ src/main/java/neluxx/no_more_ravines/NoMoreRavines.java
@@ -6,24 +6,15 @@ import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class NoMoreRavines implements ModInitializer {
 	public static final String MOD_ID = "no-more-ravines";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-
         ResourceManagerHelper.registerBuiltinResourcePack(
                 Identifier.of(MOD_ID, "datapack"),
                 FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
                 ResourcePackActivationType.ALWAYS_ENABLED
         );
-
-        LOGGER.info("No more Ravines datapack registered!");
 	}
 }
