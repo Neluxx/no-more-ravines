c03625004ba7c9c06735d7cbf2b4b4b448723d14
Neluxx <fabian.arndt96@proton.me>
2025-12-08 20:17:11 +0100

    style: remove unnecessary comments

--- src/main/java/neluxx/no_more_ravines/NoMoreRavines.java
+++ src/main/java/neluxx/no_more_ravines/NoMoreRavines.java
@@ -12,20 +12,12 @@ import org.slf4j.LoggerFactory;
 public class NoMoreRavines implements ModInitializer {
 	public static final String MOD_ID = "no-more-ravines";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 
-        // Register the datapack
         ResourceManagerHelper.registerBuiltinResourcePack(
                 Identifier.of(MOD_ID, "datapack"),
                 FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
