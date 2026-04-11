7b235039d4d4fae3dcd84d7b1bdca75d0deb79a3
Neluxx <fabian.arndt96@proton.me>
2026-04-11 20:50:37 +0200

    refactor: cleanup beet plugin

--- src/plugins/canyon.py
+++ src/plugins/canyon.py
@@ -7,16 +7,12 @@ PROBABILITY = 0 # defaults to 0.01
 
 
 def beet_default(ctx: Context):
-    base_version = ctx.meta["base_version"]
-    overlay_versions = ctx.meta["overlay_versions"]
-    vanilla = ctx.inject(Vanilla)
-
-    source = get_source(vanilla, base_version)
+    source = get_source(ctx.inject(Vanilla), ctx.meta["base_version"])
     apply_patch(ctx.data, source)
 
-    for directory, version in overlay_versions.items():
+    for directory, version in ctx.meta["overlay_versions"].items():
         overlay = ctx.data.overlays[directory]
-        source = get_source(vanilla, version)
+        source = get_source(ctx.inject(Vanilla), version)
         apply_patch(overlay, source)
 
 
@@ -26,6 +22,8 @@ def get_source(vanilla: Vanilla, version: str):
 
 def apply_patch(pack: DataPack, source):
     patched = source[CANYON].copy()
+
     config = patched.data["config"]
     config["probability"] = PROBABILITY
+
     pack[WorldgenConfiguredCarver][CANYON] = patched
