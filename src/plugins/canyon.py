0f6aaa8b8750e123c7f429bc8427f9ac45320dcb
Neluxx <fabian.arndt96@proton.me>
2026-04-13 21:01:32 +0200

    refactor: use utility class for beet plugin

--- src/plugins/canyon.py
+++ src/plugins/canyon.py
@@ -1,26 +1,18 @@
-from beet import Context, DataPack
+from beet import Context
 from beet.contrib.vanilla import Vanilla
 from beet.contrib.worldgen import WorldgenConfiguredCarver
 
+from src.plugins.utils import iterate_versions
+
 
 def beet_default(ctx: Context):
-    source = get_source(ctx.inject(Vanilla), ctx.meta["base_version"])
-    apply_patch(ctx.data, source)
+    vanilla = ctx.inject(Vanilla)
 
-    for directory, version in ctx.meta["overlay_versions"].items():
-        overlay = ctx.data.overlays[directory]
-        source = get_source(ctx.inject(Vanilla), version)
-        apply_patch(overlay, source)
+    for pack, version in iterate_versions(ctx):
+        source = vanilla.releases[version].mount("data").data[WorldgenConfiguredCarver]
+        patched = source["minecraft:canyon"].copy()
 
+        # The probability that each chunk attempts to generate carvers.
+        patched.data["config"]["probability"] = 0 # defaults to 0.01
 
-def get_source(vanilla: Vanilla, version: str):
-    return vanilla.releases[version].mount("data").data[WorldgenConfiguredCarver]
-
-
-def apply_patch(pack: DataPack, source):
-    patched = source["minecraft:canyon"].copy()
-
-    # The probability that each chunk attempts to generate carvers.
-    patched.data["config"]["probability"] = 0 # defaults to 0.01
-
-    pack[WorldgenConfiguredCarver]["minecraft:canyon"] = patched
+        pack[WorldgenConfiguredCarver]["minecraft:canyon"] = patched
